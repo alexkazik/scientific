014577ae9162deffc0b7aca49e1d5c694f646c3f
Alex Kazik <alex@kazik.de>
2022-11-12 09:51:44 +0100

    Add a trait to inc-/decrease the value of Precision

--- scientific/src/types/precision.rs
+++ scientific/src/types/precision.rs
@@ -1,3 +1,5 @@
+use core::ops::{Add, AddAssign, Sub, SubAssign};
+
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
 pub enum Precision {
   Digits(isize),
@@ -12,5 +14,45 @@ impl Precision {
 impl Default for Precision {
   fn default() -> Self {
     Precision::INTEGER
+  }
+}
+
+impl Add<isize> for Precision {
+  type Output = Precision;
+
+  fn add(self, rhs: isize) -> Self::Output {
+    match self {
+      Precision::Digits(d) => Precision::Digits(d + rhs),
+      Precision::Decimals(d) => Precision::Decimals(d + rhs),
+    }
+  }
+}
+
+impl AddAssign<isize> for Precision {
+  fn add_assign(&mut self, rhs: isize) {
+    match self {
+      Precision::Digits(d) => *d += rhs,
+      Precision::Decimals(d) => *d += rhs,
+    }
+  }
+}
+
+impl Sub<isize> for Precision {
+  type Output = Precision;
+
+  fn sub(self, rhs: isize) -> Self::Output {
+    match self {
+      Precision::Digits(d) => Precision::Digits(d - rhs),
+      Precision::Decimals(d) => Precision::Decimals(d - rhs),
+    }
+  }
+}
+
+impl SubAssign<isize> for Precision {
+  fn sub_assign(&mut self, rhs: isize) {
+    match self {
+      Precision::Digits(d) => *d -= rhs,
+      Precision::Decimals(d) => *d -= rhs,
+    }
   }
 }
