4ce9258219546486f3ab49a8e0736482c31c7f92
Alex Kazik <alex@kazik.de>
2022-11-19 11:44:12 +0100

    Rewrite the hashing
    This is a breaking change.
    
    Now hasing zero does change the hash, before this adding a zero to a
    hash did not change it and thus could not be checked.

--- scientific/src/types/hash.rs
+++ scientific/src/types/hash.rs
@@ -3,10 +3,16 @@ use core::hash::{Hash, Hasher};
 
 impl Sci {
   pub(crate) fn hash<H: Hasher>(&self, state: &mut H) {
+    // it is important to always hash at least something because otherwise
+    // calling hash would not change it and it's not distinguishable if such
+    // a call was made or not
     if !self.is_zero() {
+      // the sign is undefined for zero (could be either pos or neg)
       (self.sign.is_negative()).hash(state);
+      // data is not allowed to be accessed for zero
       Hash::hash_slice(self.data.as_slice(self.len), state);
-      self.exponent.hash(state);
     }
+    // exponent is always 1 for zero and thus consistent
+    self.exponent.hash(state);
   }
 }
