d6bd3221558cd8be78c517fcfe4b75dd80f5f56d
Alex Kazik <alex@kazik.de>
2023-08-26 09:01:20 +0200

    Split integer/float test

---
+++ scientific/tests/float_common/mod.rs
@@ -0,0 +1,123 @@
+use core::convert::TryFrom;
+use rand::prelude::SliceRandom;
+use scientific::{Error, Precision, Scientific};
+
+#[allow(clippy::type_complexity)]
+const FUNCTIONS: [(
+  &str,
+  fn(f64, f64) -> f64,
+  fn(&Scientific, &Scientific) -> Result<Scientific, Error>,
+); 4] = [
+  ("add", |a, b| a + b, |a, b| Ok(a + b)),
+  ("sub", |a, b| a - b, |a, b| Ok(a - b)),
+  ("mul", |a, b| a * b, |a, b| Ok(a * b)),
+  (
+    "div",
+    |a, b| a / b,
+    |a, b| a.div_truncate(b, Precision::F64),
+  ),
+  // rem can't be tested due to the lack of precision in float
+];
+
+pub(crate) fn float_skip<I>(numbers: I)
+where
+  I: Iterator<Item = f64>,
+{
+  let mut numbers = numbers
+    .map(|n| (n, Scientific::from_string(n.to_string()).unwrap()))
+    .collect::<Vec<(f64, Scientific)>>();
+
+  let mut rng = rand::thread_rng();
+  numbers.shuffle(&mut rng);
+
+  for (flt_a, sci_a) in numbers.iter() {
+    for (flt_b, sci_b) in numbers.iter() {
+      for (name, flt_fn, sci_fn) in FUNCTIONS.iter() {
+        let flt_result = flt_fn(*flt_a, *flt_b);
+        let sci_result = sci_fn(sci_a, sci_b);
+        let diff = diff(sci_a, sci_b, flt_result, &sci_result);
+        assert!(
+          diff < 2e-15_f64,
+          "function {name}({flt_a}, {flt_b}) -> {flt_result:?} = {sci_result:?}; diff: {diff:e}",
+        );
+      }
+      // partial_cmp
+      let flt_result = flt_a.partial_cmp(flt_b);
+      let sci_result = sci_a.partial_cmp(sci_b);
+      assert_eq!(
+        flt_result, sci_result,
+        "function {}({}, {}) -> {:?} = {:?}",
+        "partial_cmp", flt_a, flt_b, flt_result, sci_result,
+      );
+    }
+
+    // powi
+    for int_b in &[0, 1, 2, 10, 100] {
+      let flt_result = flt_a.powi(*int_b);
+      let sci_result = sci_a.powi((*int_b) as usize);
+      let diff = diff(sci_a, sci_a, flt_result, &Ok(sci_result.clone()));
+      assert!(
+        diff < 5e-15_f64,
+        "function {}({}, {}) -> {:?} = {:?}; diff: {:e}",
+        "powi",
+        flt_a,
+        int_b,
+        flt_result,
+        sci_result,
+        diff,
+      );
+    }
+    // sqrt
+    let flt_result = flt_a.sqrt();
+    let sci_result = sci_a.sqrt_rpsp(Precision::F64);
+    let diff = diff(sci_a, sci_a, flt_result, &sci_result);
+    assert!(
+      diff < 2e-15_f64,
+      "function {}({}) -> {:?} = {:?}; diff: {:e}",
+      "sqrt",
+      flt_a,
+      flt_result,
+      sci_result,
+      diff,
+    );
+    // bytes
+    let bytes = sci_a.to_bytes();
+    assert_eq!(Ok(sci_a), Scientific::from_bytes(&bytes).as_ref());
+  }
+}
+
+fn diff(sci_a: &Scientific, sci_b: &Scientific, flt: f64, sci: &Result<Scientific, Error>) -> f64 {
+  match sci {
+    Err(_) => {
+      if flt.is_finite() {
+        // Error while calculating sci, but a finite number while calculating f64 -> a mismatch
+        1.0
+      } else {
+        // Error while calculating sci (probably division by zero) and
+        // infinite or nan while calculating f64 -> probably a match
+        // (1.0 / 0.0 => infinite, 0.0 / 0.0 => nan)
+        0.0
+      }
+    }
+    Ok(sci) => {
+      if flt.is_infinite() && f64::from(sci).is_infinite() {
+        // if the sci result as a f64 and the f64 result itself is infinite then
+        // the number is too large to calculate in f64 -> match (just maybe, but can't be proven wrong)
+        0.0
+      } else {
+        match Scientific::try_from(flt) {
+          Err(_) => 1.0, // sci produced a finite value but f64 didn't -> a mismatch (infinite is handled above)
+          Ok(flt) => {
+            let max_exp = sci
+              .exponent1()
+              .max(sci_a.exponent1())
+              .max(sci_b.exponent1());
+            let n1 = sci >> max_exp;
+            let n2 = &flt >> max_exp;
+            (&(&n1 - &n2).abs()).into()
+          }
+        }
+      }
+    }
+  }
+}
---
+++ scientific/tests/integer_common/mod.rs
@@ -0,0 +1,150 @@
+use core::convert::TryFrom;
+use num_integer::Roots;
+use rand::prelude::SliceRandom;
+use scientific::{Decimals, Error, Precision, Scientific};
+
+#[allow(clippy::type_complexity)]
+const FUNCTIONS: [(
+  &str,
+  fn(i128, i128) -> Result<i128, Error>,
+  fn(&Scientific, &Scientific) -> Result<Scientific, Error>,
+); 5] = [
+  ("add", |a, b| Ok(a + b), |a, b| Ok(a + b)),
+  ("sub", |a, b| Ok(a - b), |a, b| Ok(a - b)),
+  ("mul", |a, b| Ok(a * b), |a, b| Ok(a * b)),
+  ("div", int_div, |a, b| a.div_truncate(b, Precision::INTEGER)),
+  ("rem", int_rem, |a, b| a.div_rem(b).map(|(_, r)| r)),
+];
+
+fn int_div(a: i128, b: i128) -> Result<i128, Error> {
+  if b == 0 {
+    Err(Error::DivisionByZero)
+  } else {
+    Ok(a / b)
+  }
+}
+
+fn int_rem(a: i128, b: i128) -> Result<i128, Error> {
+  if b == 0 {
+    Err(Error::DivisionByZero)
+  } else {
+    Ok(a % b)
+  }
+}
+
+pub(crate) fn integer_skip<I>(numbers: I)
+where
+  I: Iterator<Item = i128>,
+{
+  let mut numbers = numbers
+    .map(|n| (n, Scientific::from(n)))
+    .collect::<Vec<(i128, Scientific)>>();
+
+  let mut rng = rand::thread_rng();
+  numbers.shuffle(&mut rng);
+
+  for (int_a, sci_a) in numbers.iter() {
+    for (int_b, sci_b) in numbers.iter() {
+      // several functions
+      for (name, int_fn, sci_fn) in FUNCTIONS.iter() {
+        let int_result = int_fn(*int_a, *int_b);
+        let sci_result = sci_fn(sci_a, sci_b);
+        assert_eq!(
+          int_result.map(|i| Scientific::from_string(i.to_string()).unwrap()),
+          sci_result,
+          "function {name}({int_a}, {int_b}) -> {int_result:?} = {sci_result:?}",
+        );
+      }
+      // compare
+      let int_result = int_a.cmp(int_b);
+      let sci_result = sci_a.cmp(sci_b);
+      assert_eq!(
+        int_result, sci_result,
+        "function {}({}, {}) -> {:?} = {:?}",
+        "cmp", int_a, int_b, int_result, sci_result,
+      );
+      // div_rpsp
+      let big = sci_a.div_truncate(sci_b, Decimals(20)).map(|mut b| {
+        b.round_rpsp_assign(Decimals(1));
+        b
+      });
+      let fast = sci_a.div_rpsp(sci_b, Decimals(1));
+      assert_eq!(big, fast, "div_rpsp vs. div_truncate and round_rpsp");
+      // div_truncate
+      let big = sci_a.div_rpsp(sci_b, Decimals(1)).map(|mut b| {
+        b.truncate_assign(Decimals(1));
+        b
+      });
+      let fast = sci_a.div_truncate(sci_b, Decimals(1));
+      assert_eq!(big, fast, "div_truncate vs. div_rpsp and truncate");
+    }
+    // i128 via string
+    let i2s = sci_a;
+    let i2s_str = Scientific::from_string(int_a.to_string()).unwrap();
+    assert_eq!(i2s, &i2s_str, "conversion from i128");
+    // i128
+    let s2i = i128::try_from(i2s);
+    assert_eq!(Ok(*int_a), s2i, "conversion from/to i128");
+    // i64
+    let i2s = Scientific::from(*int_a as i64);
+    let s2i = i64::try_from(&i2s);
+    assert_eq!(Ok(*int_a as i64), s2i, "conversion from/to i64");
+    // i32
+    let i2s = Scientific::from(*int_a as i32);
+    let s2i = i32::try_from(&i2s);
+    assert_eq!(Ok(*int_a as i32), s2i, "conversion from/to i32");
+    // sqrt
+    if *int_a >= 0 {
+      // v1
+      let sci_cubed = sci_a * sci_a;
+      let sci_result = sci_cubed.sqrt_truncate(Decimals(100));
+      assert_eq!(
+        Ok(sci_a.clone()),
+        sci_result,
+        "function {}({}) -> {:?} = {:?}",
+        "sqrt_v1",
+        int_a * int_a,
+        int_a,
+        sci_result,
+      );
+      // v1.2
+      let sci_cubed = sci_a * sci_a;
+      let sci_result = sci_cubed.sqrt_rpsp(Decimals(1));
+      assert_eq!(
+        Ok(sci_a.clone()),
+        sci_result,
+        "function {}({}) -> {:?} = {:?}",
+        "sqrt_v1.2",
+        int_a * int_a,
+        int_a,
+        sci_result,
+      );
+      // v2
+      let int_result = int_a.sqrt();
+      let sci_result = sci_a.sqrt_truncate(Decimals(0));
+      assert_eq!(
+        Ok(Scientific::from(int_result)),
+        sci_result,
+        "function {}({}) -> {:?} = {:?}",
+        "sqrt_v2",
+        int_a,
+        int_result,
+        sci_result,
+      );
+      // v2.1
+      let int_result = int_a.sqrt();
+      let sci_result = sci_a
+        .sqrt_rpsp(Decimals(0))
+        .map(|s| s.truncate(Decimals(0)));
+      assert_eq!(
+        Ok(Scientific::from(int_result)),
+        sci_result,
+        "function {}({}) -> {:?} = {:?}",
+        "sqrt_v2",
+        int_a,
+        int_result,
+        sci_result,
+      );
+    }
+  }
+}
