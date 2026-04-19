bd52d167b0666de52886ef73def6c2e3c228d9cf
Alex Kazik <alex@kazik.de>
2026-04-19 10:07:31 +0200

    Improve check script

--- check.sh
+++ check.sh
@@ -7,6 +7,9 @@
 # Usage: `./check.sh` or `./check.sh <toolchain>`
 # If the toolchain is omitted `+nightly`,`+stable` and `+1.58.1` is used, `+stable` or `+beta` are the most common alternatives
 
+set -e # exit on error
+set -x # show commands executed
+
 TOOLCHAIN=${1:-+1.65.0}
 echo Using toolchain $TOOLCHAIN
 
@@ -14,36 +17,36 @@ echo Using toolchain $TOOLCHAIN
 cargo $TOOLCHAIN update
 
 # builds (std+arc+debug+macro, std, nothing)
-cargo $TOOLCHAIN build --release --all-features --tests || exit 1
-cargo $TOOLCHAIN build --release --no-default-features --features std --tests || exit 1
-cargo $TOOLCHAIN build --release --no-default-features || exit 1
+cargo $TOOLCHAIN build --release --all-features --tests
+cargo $TOOLCHAIN build --release --no-default-features --features std --tests
+cargo $TOOLCHAIN build --release --no-default-features
 
 TOOLCHAIN=${1:-+nightly}
 echo Using toolchain $TOOLCHAIN
 
 # builds (std+arc+debug+macro, std, nothing)
-cargo $TOOLCHAIN build --release --all-features --tests || exit 1
-cargo $TOOLCHAIN build --release --no-default-features --features std --tests || exit 1
-cargo $TOOLCHAIN build --release --no-default-features --tests || exit 1
+cargo $TOOLCHAIN build --release --all-features --tests
+cargo $TOOLCHAIN build --release --no-default-features --features std --tests
+cargo $TOOLCHAIN build --release --no-default-features --tests
 
 # clippy (std+arc+debug+macro, std, nothing)
-cargo $TOOLCHAIN clippy --release --all-features --tests -- -D warnings || exit 1
-cargo $TOOLCHAIN clippy --release --no-default-features --features std --tests -- -D warnings || exit 1
-cargo $TOOLCHAIN clippy --release --no-default-features -- -D warnings || exit 1
+cargo $TOOLCHAIN clippy --release --all-features --tests -- -D warnings
+cargo $TOOLCHAIN clippy --release --no-default-features --features std --tests -- -D warnings
+cargo $TOOLCHAIN clippy --release --no-default-features -- -D warnings
 
 # update formatting
-cargo $TOOLCHAIN fmt --all || exit 1
+cargo $TOOLCHAIN fmt --all
 
 # ensure that the library doesn't use re-exports
 RUSTFLAGS="--cfg no_re_export" cargo build -p scientific
 
 # update readme
-( cd scientific && cargo rdme --force ) || exit 1
+( cd scientific && cargo rdme --force )
 
 # create docs
 if test "$TOOLCHAIN" = "+nightly"
 then
-  RUSTDOCFLAGS="--cfg docsrs" cargo +nightly doc -p scientific --all-features || exit 1
+  RUSTDOCFLAGS="--cfg docsrs" cargo +nightly doc -p scientific --all-features
 else
   echo "Skipping 'cargo doc' with doc_cfg since it's only available on nightly"
 fi
@@ -52,5 +55,7 @@ TOOLCHAIN=${1:-+stable}
 echo Using toolchain $TOOLCHAIN
 
 # tests (std+arc+debug+macro, only debug [skip doctest since they require macro])
-cargo $TOOLCHAIN test --locked --release --all-features -- --include-ignored || exit 1
-cargo $TOOLCHAIN test --locked --release --no-default-features --features debug --lib --bins --tests -- --include-ignored || exit 1
+cargo $TOOLCHAIN test --locked --release --all-features -- --include-ignored
+cargo $TOOLCHAIN test --locked --release --no-default-features --features debug --lib --bins --tests -- --include-ignored
+
+typos
